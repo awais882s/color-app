5ef63cda323cb90c455dc5f7d71301c4abc156f4
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-20 16:41:49 +0500

    color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import ""
-
+import React from "react";
+import { useState } from "react";
 function App() {
+  const [color, setcColor] = useState("#fff")
+  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+  const getRandomNumbers = () => {
+    return Math.floor(Math.random() * hex.length)
+  }
+  const handleClick = i => {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[getRandomNumbers()];
+      setcColor(hexColor)
+
+    }
+
+
+
+  }
   return (
     <>
-     
+      <h1>Color App</h1>
+      <button type="button" className="btn btn-primary ms-2" onClick={handleClick}>Color App</button>
+
     </>
   );
 }
