118d4b5b934881a0d07c75324da9605d1451ad62
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-20 22:35:19 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
-
 export default function App() {
   const [color, setColor] = useState("#fff");
   const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
