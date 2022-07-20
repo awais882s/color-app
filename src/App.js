33b2b0c7c998f9da533c01840997280c51942674
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-20 22:29:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { useState } from "react";
-import Helmet from "react-helmet";
-function App() {
-  const [color, setcColor] = useState("#fff")
-  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
-  const getRandomNumbers = () => {
-    return Math.floor(Math.random() * hex.length)
-  }
+import React, { useState } from "react";
+import { Helmet } from "react-helmet";
+
+export default function App() {
+  const [color, setColor] = useState("#fff");
+  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * hex.length);
+  };
+
   const handleClick = i => {
     let hexColor = "#";
     for (let i = 0; i < 6; i++) {
-      hexColor += hex[getRandomNumbers()];
-      setcColor(hexColor)
-      console.log(color);
-
+      hexColor += hex[getRandomNumber()];
+      setColor(hexColor);
+      window.document.body.style.backgroundColor = color;
     }
-  }
+  };
+
+
+
   return (
     <>
-      <h1>Color App</h1>
-      <Helmet>
-        <style>{`body {background-color: ${color}}`}</style>
-      </Helmet>
-      <button type="button" className="btn btn-primary ms-2" value="Click Me" onClick={handleClick}></button>
 
+      <div className="text-center">
+        <button onClick={handleClick} value="Click Me" >Click Me</button>
+      </div>
     </>
   );
 }
-
-export default App;
