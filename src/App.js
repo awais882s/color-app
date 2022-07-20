2720d9e1af03fa1d33a295837c5bb8cbec98c8c4
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-07-20 22:35:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Helmet } from "react-helmet";
 export default function App() {
   const [color, setColor] = useState("#fff");
   const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-
   const getRandomNumber = () => {
     return Math.floor(Math.random() * hex.length);
   };
